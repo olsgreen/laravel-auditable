9ed7664616f0a0c587663e2f356ea22cab45cea9
Chris Lewis <chris@chrisnetonline.com>
2014-10-24 14:38:08 -0400

    Fixing user authenicated check.
    Fixed a bug where if your \User class is namespaced it isn't logging the user_id.

--- src/Olsgreen/Auditable/Auditable.php
+++ src/Olsgreen/Auditable/Auditable.php
@@ -2,9 +2,9 @@
 
 /**
  * Auditable
- * An easy to implement attribute audit 
+ * An easy to implement attribute audit
  * log for Eloquent ORM in Laravel 4.X.
- * 
+ *
  * @author Oliver Green <green2go@gmail.com>
  * @version 1.0.0
  * @license http://opensource.org/licenses/MIT
@@ -15,16 +15,16 @@ use Illuminate\Database\Eloquent\Model;
 class Auditable {
 
 	/**
-	 * Gets Builder instance with predefined query 
+	 * Gets Builder instance with predefined query
 	 * parameters for the input model parameter.
-	 * 
+	 *
 	 * @param   Illuminate\Database\Eloquent\Model $model
 	 * @return  Illuminate\Database\Eloquent\Builder
 	 */
 	public function get(Model $model)
 	{
 		// Grab the models PK
-		$primarykey = (isset($model->primarykey)) 
+		$primarykey = (isset($model->primarykey))
 							? $model->primarykey : 'id';
 
 		return Changeset::where('object_type', '=', get_class($model))
@@ -32,9 +32,9 @@ class Auditable {
 	}
 
 	/**
-	 * Records differences between dirty and 
+	 * Records differences between dirty and
 	 * clean attributes to the database.
-	 * 
+	 *
 	 * @param  Illuminate\Database\Eloquent\Model $model
 	 * @return boolean Whether there was a Changeset created
 	 */
@@ -43,7 +43,7 @@ class Auditable {
 		$dirty = $model->getDirty();
 
 		// Remove dirty keys not in the auditable array
-		if (isset($model->auditable) && is_array($model->auditable) 
+		if (isset($model->auditable) && is_array($model->auditable)
 				&& !in_array('*', $model->auditable)) {
 			$dirty = array_intersect_key($dirty, array_flip($model->auditable));
 		}
@@ -51,21 +51,21 @@ class Auditable {
 		if ($model->exists && count($dirty) > 0) {
 
 			// Grab the models PK
-			$primarykey = (isset($model->primarykey)) 
+			$primarykey = (isset($model->primarykey))
 								? $model->primarykey : 'id';
 
 			/*
 			* -------------------------------------------------
 			* Create the Changeset
 			* -------------------------------------------------
-			* We have to improvise a little here as we 
-			* don't want to depend on models having 
+			* We have to improvise a little here as we
+			* don't want to depend on models having
 			* relationships with the Auditable Changeset model.
 			*/
 			$cs = new Changeset;
 			$cs->object_type = get_class($model); // Manual
 			$cs->object_id = $model->$primarykey; // Manual
-			$cs->user_id = (\Auth::user() instanceof \User) ? \Auth::user()->id : 0;
+			$cs->user_id = (\Auth::check()) ? \Auth::user()->id : 0;
 			$cs->save();
 
 			// Add the Changeset changes
